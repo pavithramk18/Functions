de5e8789b28599466cbec306a2839d57bafc5604
pavithramk18 <pavithram.kumaresan@gmail.com>
2023-08-01 19:29:43 +0530

    Array done

--- index.js
+++ index.js
@@ -23,7 +23,8 @@
 // var array = [5,4,3,2,1];
 // var oddarray = [];
 // var odd = function (array) {
-//   for (let i = 0; i < array.length; i++) {
+//   for (let i = 0; i < array.length; i++) 
+//{
 //     if (array[i] % 2 != 0) {
 //       oddarray.push(array[i]);
 //     }
@@ -35,25 +36,29 @@
 // //Find odd elements in the given array by using 
 // //IIFE (Immediately Invoked Function Expression)
 
-(function odd(array) {
-  var oddarray = [];
-  for (var i = 0; i < array.length; i++) {
-    if (array[i] % 2 != 0) {
-      oddarray.push(array[i]);
-    }
-  }
-  console.log(oddarray);
-})([5,4,3,2,1]);
-
-// //Find odd number in an array by using Arrow Function (ES6)
-
-// const oddar= (array) => {
+// (function odd(array) 
+//{
 //   var oddarray = [];
-//   for (var i = 0; i < array.length; i++) {
+//   for (var i = 0; i < array.length; i++)
+// {
 //     if (array[i] % 2 != 0) {
 //       oddarray.push(array[i]);
 //     }
 //   }
-//   return oddarray;
-// };
-// console.log(oddar([5,4,3,2,1]));
+//   console.log(oddarray);
+// })([5,4,3,2,1]);
+
+// //Find odd number in an array by using Arrow Function (ES6)
+
+const oddar= (array) => {
+  var oddarray = [];
+  for (var i = 0; i < array.length; i++) 
+  {
+    if (array[i] % 2 != 0)
+     {
+      oddarray.push(array[i]);
+    }
+  }
+  return oddarray;
+};
+console.log(oddar([5,4,3,2,1]));
